1c45195da9be45b7002edbef564f12cfac86f863
sama <sama@sotoon.ir>
2023-04-14 00:42:15 +0330

    create model task

--- TODO_django/todo_list/models.py
+++ TODO_django/todo_list/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class BaseModel(models.Model):
+    creation_date = models.DateTimeField()
+    last_modified = models.DateTimeField()
+    is_deleted = models.DateTimeField()
+
+    class Meta:
+        abstract = True  # Set this model as Abstract
+
+class Task(BaseModel):
+
+    title = models.CharField()
+    description = models.TextField()
+    is_done = models.BooleanField()
